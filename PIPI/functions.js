8c984e2d5cdffbbe85843646ab0582010d6fe8c8
jnguarin <142811908+jnguarin@users.noreply.github.com>
2023-12-02 14:35:05 -0500

    s

--- PIPI/functions.js
+++ PIPI/functions.js
@@ -1,13 +1,17 @@
 // Compares Standarized Dicts 
 
 function dictCompare(dict1, dict2){
+    let dict1Norm = dict1;
     let keys = commonKeys(dict1,dict2);
-    let dict1Norm = normalizador(dict1,keys);
-    let dict2Norm = normalizador(dict2,keys);
+    // let dict1Norm = normalizador(dict1,keys);
+    // let dict2Norm = normalizador(dict2,keys);
 
-    for(let i in keys){ 
-        console.log(dict1Norm[i]);
+    for(let i in keys){
+        let coord1X = dict1Norm[keys[i]];
+        console.log(coord1X);
     }
 
 
 }
+
+dictCompare(inputDict,referenceDict);
